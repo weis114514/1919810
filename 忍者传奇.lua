9abf78f1bb341b40b9c75c79cd8ccf67f02ccc2b
weis114514 <zx3606359397@163.com>
2024-08-12 17:30:54 +0800

    Create 忍者传奇.lua

---
+++ 忍者传奇.lua
@@ -0,0 +1,75 @@
+local ada=Instance.new("ScreenGui")
+local bda=Instance.new("Frame")local cda=Instance.new("TextButton")
+local dda=Instance.new("TextButton")local __b=Instance.new("TextButton")
+local a_b=Instance.new("TextButton")local b_b=Instance.new("TextButton")
+local c_b=Instance.new("TextButton")local d_b=Instance.new("TextButton")
+local _ab=Instance.new("TextButton")local aab=Instance.new("TextButton")
+local bab=Instance.new("TextButton")local cab=Instance.new("TextButton")
+local dab=Instance.new("TextButton")local _bb=Instance.new("TextButton")
+local abb=Instance.new("TextButton")local bbb=Instance.new("TextButton")
+local cbb=Instance.new("TextButton")local dbb=Instance.new("TextButton")
+local _cb=Instance.new("TextButton")local acb=Instance.new("TextButton")
+local bcb=Instance.new("TextButton")local ccb=Instance.new("TextLabel")
+ada.Parent=game.Players.LocalPlayer:WaitForChild("PlayerGui")bda.Parent=ada;bda.BackgroundColor3=Color3.new(1,1,1)
+bda.BorderSizePixel=4;bda.Position=UDim2.new(0.19525066,0,0.497435898,0)
+bda.Size=UDim2.new(0,504,0,304)cda.Parent=bda
+cda.BackgroundColor3=Color3.new(0.501961,0.501961,0.0117647)
+cda.BorderColor3=Color3.new(0.0627451,0.501961,0.00392157)cda.BorderSizePixel=3
+cda.Position=UDim2.new(0.0218253974,0,0.914473712,0)cda.Size=UDim2.new(0,65,0,18)
+cda.Font=Enum.Font.GothamBold;cda.Text="重置"cda.TextColor3=Color3.new(0,0,0)
+cda.TextSize=18;dda.Parent=bda
+dda.BackgroundColor3=Color3.new(0.501961,0.501961,0.0117647)
+dda.BorderColor3=Color3.new(0.0627451,0.501961,0.25098)dda.BorderSizePixel=3
+dda.Position=UDim2.new(0.174603179,0,0.914473712,0)dda.Size=UDim2.new(0,106,0,21)
+dda.Font=Enum.Font.GothamBold;dda.Text="自动坏人"dda.TextColor3=Color3.new(0,0,0)
+dda.TextSize=14;__b.Parent=bda
+__b.BackgroundColor3=Color3.new(0.501961,0.501961,0.0117647)
+__b.BorderColor3=Color3.new(0.0627451,0.501961,0.25098)__b.BorderSizePixel=3
+__b.Position=UDim2.new(0.412698418,0,0.917763174,0)__b.Size=UDim2.new(0,42,0,21)
+__b.Font=Enum.Font.GothamBold;__b.Text="OFF"__b.TextColor3=Color3.new(0,0,0)__b.TextSize=14
+a_b.Parent=bda
+a_b.BackgroundColor3=Color3.new(0.501961,0.501961,0.0117647)
+a_b.BorderColor3=Color3.new(0.0627451,0.501961,0.25098)a_b.BorderSizePixel=3
+a_b.Position=UDim2.new(0.521825373,0,0.914473712,0)a_b.Size=UDim2.new(0,106,0,21)
+a_b.Font=Enum.Font.GothamBold;a_b.Text="自动好人"a_b.TextColor3=Color3.new(0,0,0)
+a_b.TextSize=14;b_b.Parent=bda
+b_b.BackgroundColor3=Color3.new(0.501961,0.501961,0.0117647)
+b_b.BorderColor3=Color3.new(0.0627451,0.501961,0.25098)b_b.BorderSizePixel=3
+b_b.Position=UDim2.new(0.759920597,0,0.917763174,0)b_b.Size=UDim2.new(0,42,0,21)
+b_b.Font=Enum.Font.GothamBold;b_b.Text="OFF"b_b.TextColor3=Color3.new(0,0,0)b_b.TextSize=14
+c_b.Parent=bda
+c_b.BackgroundColor3=Color3.new(0.501961,0.501961,0.0117647)
+c_b.BorderColor3=Color3.new(0.0627451,0.501961,0.00392157)c_b.BorderSizePixel=3
+c_b.Position=UDim2.new(0.0238095243,0,0.802631557,0)c_b.Size=UDim2.new(0,65,0,18)
+c_b.Font=Enum.Font.GothamBold;c_b.Text="最大JP"c_b.TextColor3=Color3.new(0,0,0)
+c_b.TextSize=18;d_b.Parent=bda
+d_b.BackgroundColor3=Color3.new(0.501961,0.501961,0.0117647)
+d_b.BorderColor3=Color3.new(0.0627451,0.501961,0.25098)d_b.BorderSizePixel=3
+d_b.Position=UDim2.new(0.174603179,0,0.799342096,0)d_b.Size=UDim2.new(0,106,0,21)
+d_b.Font=Enum.Font.GothamBold;d_b.Text="无限隐身"d_b.TextColor3=Color3.new(0,0,0)
+d_b.TextSize=14;_ab.Parent=bda
+_ab.BackgroundColor3=Color3.new(0.501961,0.501961,0.0117647)
+_ab.BorderColor3=Color3.new(0.0627451,0.501961,0.25098)_ab.BorderSizePixel=3
+_ab.Position=UDim2.new(0.521825373,0,0.799342096,0)_ab.Size=UDim2.new(0,106,0,21)
+_ab.Font=Enum.Font.GothamBold;_ab.Text="解锁所有岛屿"_ab.TextColor3=Color3.new(0,0,0)
+_ab.TextSize=14;aab.Parent=bda
+aab.BackgroundColor3=Color3.new(0.501961,0.501961,0.0117647)
+aab.BorderColor3=Color3.new(0.0627451,0.501961,0.25098)aab.BorderSizePixel=3
+aab.Position=UDim2.new(0.176587299,0,0.684210539,0)aab.Size=UDim2.new(0,106,0,21)
+aab.Font=Enum.Font.GothamBold;aab.Text="删除通知"aab.TextColor3=Color3.new(0,0,0)
+aab.TextSize=10;bab.Parent=bda
+bab.BackgroundColor3=Color3.new(0.501961,0.501961,0.0117647)
+bab.BorderColor3=Color3.new(0.0627451,0.501961,0.25098)bab.BorderSizePixel=3
+bab.Position=UDim2.new(0.521825373,0,0.684210539,0)bab.Size=UDim2.new(0,106,0,21)
+bab.Font=Enum.Font.GothamBold;bab.Text="大领袖全部"bab.TextColor3=Color3.new(0,0,0)
+bab.TextSize=14;cab.Parent=bda
+cab.BackgroundColor3=Color3.new(0.501961,0.501961,0.0117647)
+cab.BorderColor3=Color3.new(0.0627451,0.501961,0.25098)cab.BorderSizePixel=3
+cab.Position=UDim2.new(0.0297619049,0,0.575657904,0)cab.Size=UDim2.new(0,106,0,21)
+cab.Font=Enum.Font.GothamBold;cab.Text="自动摆剑"cab.TextColor3=Color3.new(0,0,0)
+cab.TextSize=15;dab.Parent=bda
+dab.BackgroundColor3=Color3.new(0.501961,0.501961,0.0117647)
+dab.BorderColor3=Color3.new(0.0627451,0.501961,0.25098)dab.BorderSizePixel=3
+dab.Position=UDim2.new(0.267857134,0,0.575657904,0)dab.Size=UDim2.new(0,106,0,21)
+dab.Font=Enum.Font.GothamBold;dab.Text="自动出售"dab.TextColor3=Color3.new(0,0,0)
+dab.TextSize=1
